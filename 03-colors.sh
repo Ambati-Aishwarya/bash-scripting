3251517803c5de08d82294b7691f1645a115a803
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 15:56:20 +0530

    print statements

--- 03-colors.sh
+++ 03-colors.sh
@@ -11,10 +11,17 @@
 # white      37         47
 # black      30         40
 
-#syntax: echo -e "\e[COL-CODEmMESSAGE"
+#syntax: echo -e "\e[COL-CODEm MESSAGE"
 
 #echo -e "\e[colorcode MSG \e[0m]"
 echo -e "\e[32m I am printing green color \e[0m"
 echo -e "\e[31m I am printing red color \e[0m"
 echo -e "\e[33m I am printing yellow color \e[0m"
 echo -e "\e[34m I am printing blue color \e[0m"
+
+#printing with background color
+#echo -e "\e[backgroud;colorcodem Message \e[0m"
+
+echo -e "\e[43;31printing red on yelloe color \e[0m"
+
+
