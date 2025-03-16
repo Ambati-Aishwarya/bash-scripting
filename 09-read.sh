76b5a2b5dc51854423c0fc284a42dc0f2ef6cf3f
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 17:50:26 +0530

    print statements

---
+++ 09-read.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "this script will proceed only by taking input from the user interactively on the terminal while executing the script"
+
+read -p "enter your name of the person running the script: " name
+echo "name of the person running the script is $name"
