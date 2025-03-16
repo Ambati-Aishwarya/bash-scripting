5754247b1c98c51dd8d144354bc971e45e766a71
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 17:43:34 +0530

    print statements

---
+++ 08-quotes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# " " double quotes
+# ' ' single quotes: it will eliminate the power of special characters
+a=100
+echo "demo on quotes"
+echo '$?'
+echo "value of  is $?"
+echo "value of a is $a"
+echo 'value of a is ${a}'
