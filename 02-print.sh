eb2b955ad28dec4f84a39dac64fd2ef62b797b12
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 15:23:02 +0530

    print statements

--- 02-print.sh
+++ 02-print.sh
@@ -3,3 +3,12 @@ echo hai
 echo hello
 echo welcome
 echo Devsecops
+
+
+# special characters to do something
+# /n: move the cursor to the next line
+# /t: move the cursor to the next tab
+# /b: move the cursor to the backspace
+# /r: move the cursor to the beginning of the line
+
+echo "hi /n hello /n welcome /n Devsecops"
