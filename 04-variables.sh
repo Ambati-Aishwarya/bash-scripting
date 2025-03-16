ac00eaf04b6b6c3ef5b4aea0f36464c57a251e4d
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 16:11:17 +0530

    print statements

---
+++ 04-variables.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+# variables:
+#this is very important concept in any programming language or technology
+#without this ,it is quite tough to see the quqality of thr code or script
+#just by looking at the scripts we can say how good the script is
+#variables are used to store the data and use it whwnever required
+
+#in bash there is no cobcept of data types, by default everything is string
+#in bash we dont have to declare the variables.we can directly assign the value to the variables
+#syntax: VAR=DATA
+#a=10
+#how to print the variable: $VAR_NAME or ${VAR_NAME}
+
+a=10
+echo "printing the value of a $a"
