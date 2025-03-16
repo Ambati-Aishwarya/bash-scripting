2426b6c5a0d7c409cc78d10725bf271033e10c01
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 16:25:20 +0530

    print statements

--- 04-variables.sh
+++ 04-variables.sh
@@ -13,4 +13,8 @@
 
 a=10
 echo  "printing the value of a $a"
-echo  "printing the value of a $b"
+echo  "printing the value of b $b"
+#when we try to print the variable which is not defined, it will print empty value
+#$a 0r ${a} or"${a}" are same
+echo 'printing the value of a ${a}'
+#when you enclose the variable with single quotes, it will print the variable name as it is.thatmeans single quotes will eliminate the power of special variables
