ca140bd7fa6f271b7c771646a38e05f7585d6db0
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-16 15:49:13 +0530

    print statements

--- 03-colors.sh
+++ 03-colors.sh
@@ -15,3 +15,6 @@
 
 #echo -e "\e[colorcode MSG \e[0m]"
 echo -e "\e[32m I am printing green color \e[0m"
+echo -e "\e[31m I am printing red color \e[0m"
+echo -e "\e[33m I am printing yellow color \e[0m"
+echo -e "\e[34m I am printing blue color \e[0m"
