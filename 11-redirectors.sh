6632fdc042725117e55cad5330eb3009915bd68d
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-22 11:20:04 +0530

    statements

---
+++ 11-redirectors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#redirctors
+#how to take the inputs ,how to give the outputs, how to handle the errors
+#redirectors are 2 types in the bash
+#1. input redirectors (<) we can use this to take the inputs from the file: < ex: sudo mysql </tmp/studentapp.sql
+#2. output redirectors (>> or >) we can use this to give the outputs to the file: . or 1> or >> or &>(>> appends the output to the file, > overwrites the output to the file, &> appends the output to the file)
+#2. output redirectors
