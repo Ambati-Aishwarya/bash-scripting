0c424920be2c47448bac27cab9d4561f2c0d756f
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-22 12:26:32 +0530

    statements

--- 13-if-if-else.sh
+++ 13-if-if-else.sh
@@ -17,15 +17,15 @@
     #demo on else if condition
     if [ "$ACTION" == "start" ];
     then
-        echo  -e " \[32m starting the service \e[0m"
+        echo  -e " \e[32m starting the service \e[0m"
         
     elif [ "$ACTION" == "stop" ];
     then
-        echo -e " \[37m stopping the service \e[0m"
+        echo -e " \e[37m stopping the service \e[0m"
 
     elif [ "$ACTION" == "restart" ];
     then
-        echo -e "\[34m restarting the service \e[0m"
+        echo -e "\e[34m restarting the service \e[0m"
     else
         echo "valid arguements are start 0r stop or restart"
         echo -e "ex: \n\t \e[33m bash $0 start \e[0m"
