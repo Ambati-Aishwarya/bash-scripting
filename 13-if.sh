b54b4157cc3b6822f8320ae766617cd98f37b6ab
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-22 12:08:56 +0530

    statements

--- 13-if.sh
+++ 13-if.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-    echo -e "demo on if conditions"
+ #   echo -e "demo on if conditions"
 
-    ACTION=$1
-    if [ "$ACTION" == "start" ];
-    then                       #the commands will be only executed if condition is true
-        echo "starting the service"
-    else
-        echo  "valid arguements are start"
-        echo  -e "ex: \n\t \e[33m bash $0 start \e[0m"    
+  #  ACTION=$1
+   # if [ "$ACTION" == "start" ];
+   # then                       #the commands will be only executed if condition is true
+    #    echo "starting the service"
+   # else
+   #     echo  "valid arguements are start"
+    #    echo  -e "ex: \n\t \e[33m bash $0 start \e[0m"    
     
-    fi
+   # fi
 
+   # echo -e "script completed"
+
+    #demo on else if condition
+    if [ "$ACTION" == "start" ];
+    then
+        echo "starting the service"
+    elif [ "$ACTION" == "stop" ];
+    then
+        echo "stopping the service"
+    elif [ "$ACTION" == "restart" ];
+    then
+        echo "restarting the service"
+    else
+        echo "valid arguements are start/stop/restart"
+        echo -e "ex: \n\t \e[33m bash $0 start \e[0m"
+    fi
+   
     echo -e "script completed"
 
 
